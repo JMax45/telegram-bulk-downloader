8e276f467552821f5568a8074b61bd588322c52a
JMax45 <maxmailru.45@gmail.com>
2023-07-07 16:31:49 +0000

    fix: improve filename extraction

--- src/helpers/getFilenameExtension.ts
+++ src/helpers/getFilenameExtension.ts
@@ -1,6 +1,12 @@
 import { Api } from 'telegram';
 import mime from 'mime-types';
 
+const extractExtensionFromFilename = (filename: string) => {
+  const split = filename.split('.');
+  if (split.length <= 1) return false;
+  return split[split.length - 1];
+};
+
 const getFilenameExtension = (msg: Api.Message) => {
   switch (msg.media?.className) {
     case 'MessageMediaPhoto':
@@ -11,6 +17,16 @@ const getFilenameExtension = (msg: Api.Message) => {
         typeof mime.extension(msg.media.document.mimeType) === 'string'
       )
         return mime.extension(msg.media.document.mimeType) as string;
+      if (
+        msg.media.document?.className === 'Document' &&
+        msg.media.document.attributes
+      ) {
+        const attribute = msg.media.document.attributes.find(
+          (e) => e.className === 'DocumentAttributeFilename'
+        ) as Api.DocumentAttributeFilename | undefined;
+        if (attribute)
+          return extractExtensionFromFilename(attribute.fileName) || '';
+      }
     default:
       throw new Error('Failed to extract extension');
   }
